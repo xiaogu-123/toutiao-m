daf711cf7319e7a11497609916a69d3fe13e6b0c
GGboy <1976343682@qq.com>
2021-12-24 00:34:21 +0800

    Vant

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
 import './styles/index.less'
+
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
