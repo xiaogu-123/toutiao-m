02ba7327f340e3437b09f9d5730a52fde3698700
GGboy <1976343682@qq.com>
2022-01-03 02:29:57 +0800

    首页navBar

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -18,4 +31,20 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
+</style>
